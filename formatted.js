d3bc585378be73beb4924e74dfb86d5611256e28
Sander-Brilman <brilmansander@gmail.com>
2022-09-28 21:51:14 +0200

    finished + added readme

---
+++ formatted.js
@@ -0,0 +1,90 @@
+// 
+// Formatted & commented version of the code
+// 
+
+
+// variable deflations
+let executeScript   = true;
+let answers         = null;
+
+// function for clicking the Check & Next button
+const checkAndNext = function() {
+    $('#btn_controleer').removeAttr('disabled');
+	$('#btn_controleer').removeAttr('disabled');
+	setTimeout($('#btn_controleer').click(), 1000);
+	setTimeout($('#btn_volgende').click(), 1000);
+}
+
+// insert created by banner
+$('.cover_header').css({'height': 'auto'});
+$('.magazine_page_largetitle2')
+.css({'font-size': '35px', 'font-weight': 'bold', 'background': 'white', 'padding': '25px', 'border-bottom': 'red solid 5px'})
+.html(`
+    Lester Cheat <span style="color: red;">Remastered</span><br><br>
+
+    Gemaakt door <a href="https://sanderbrilman.nl" target="_blank" style="text-decoration: underline;">Sander Brilman</a><br>
+    <a href="https://github.com/Sander-Brilman/lester_cheat_remastered" target="_blank" style="text-decoration: underline;">- Github repo</a><br>
+    <a href="https://github.com/leslmosnk/gps-cheater-script" target="_blank" style="text-decoration: underline;">- Origineel</a><br>
+`);
+
+// press exit on the last page
+if ($('a[data-page-id="end"]').length > 0) {
+	setTimeout(window.open($('a[data-page-id="end"]').attr('href'), '_self'), 2000);
+    executeScript = false;
+}
+
+// press next on any non-exercise pages
+if ($('.cover_header_startbtn').length > 0) {
+    window.open($('.cover_header_startbtn').attr('href'), '_self');
+    executeScript = false;
+}
+
+// press next on exercise pages that are already answered
+let nextButton = $('#btn_volgende');
+if (($('h5').length != 1 || $('h5').text() != 'Instructions') ||
+    (!nextButton.attr('disabled') !== 'undefined' && !nextButton.attr('disabled') !== false)) 
+{
+    nextButton.click();
+    executeScript = false;
+}
+
+// get & send the answers if no button has been pressed yet
+if (executeScript) {
+    $.ajax({
+        type: 'POST',
+        url: '/werkvormen/answer.php',
+        async: false,
+        data: {
+            'pwid': $('#pw_id').val(),
+        },
+        context: document.body,
+        success: function(data) {
+            answers = JSON.parse(data);
+        }
+    });
+    
+    if (typeof answers[0] !== 'string' && !(answers[0] instanceof String)) {
+        // 
+        // exceptions where a different answer format is required
+        // 
+
+        if (!isNaN(answers)) {// radio buttons
+
+            $(`#box${answers} .rradio`).click();
+
+        } else if ($('.answer30').length > 0) {// text fields
+
+            // do nothing, empty answers for text fields are correct somehow..
+
+        } else {
+            $('.magazine_page_largetitle2').css('color', 'red').html('Automatisch beantwoorden mislukt.<br><br>Kijk in de console voor de antwoorden en voer ze handmatig in.');
+            console.log('andwoorden: ', answers);
+        }
+
+    } else {
+        // send the correct answers back to the server
+        send_answer($('#pw_id').val(), answers.join('+'));
+    }
+
+    checkAndNext();
+}
---
+++ readme.md
@@ -0,0 +1,13 @@
+# Lester Cheat Remastered
+
+#### Het gps-cheater script (beter bekent als de **Lester Cheat**) is verbeterd!<br>
+Door een nieuwe manier van antwoorden stelen zijn bijna alle opdrachten nu door het script te maken en krijg je heel makkelijk en snel 100% op je opdrachten.
+
+## Hoe gebruik ik het?
+Kopieer en plak de code uit de `minified.js` file in de **console** van de **devtools** (f12 voor chrome)
+
+## Credits
+
+De [**Lester Cheat Remastered**](https://github.com/Sander-Brilman/lester_cheat_remastered) is gemaakt door [Sander Brilman](https://sanderbrilman.nl)
+<br>
+Het originele cheater-script is gemaakt door Lester Morsink en is [hier](https://github.com/leslmosnk/gps-cheater-script) te vinden
